f68818784b696e73aa6c16e618884fea57cbf5ed
atharianr <atharianrahmadani@gmail.com>
2022-05-17 21:12:11 +0700

    adding mysql to login & register

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,78 +2,105 @@ const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const connection = require('../configs/database');
 const Users = require('../models/user');
 const Plants = require('../models/plant');
 
 // REGISTER/SIGNUP
 exports.user_signup = (req, res, next) => {
-  const user = Users.find(user => user.email == req.body.email)
-  if (user !== undefined) {
-    return res.status(409).json({
-      message: 'Email already exists'
-    });
-  } else {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-      if (err) {
-        return res.status(500).json({
-          error: err
-        });
-      } else {
-        const newUser = {
-          _id: nanoid(16),
-          name: req.body.name,
-          email: req.body.email,
-          password: hash,
-          my_plant: [],
-          satisfaction_rate: 0
-        };
+  const queryCheckUser = 'SELECT * FROM tandur_coba.tandur_user WHERE email = ?'
+  connection.query(queryCheckUser, req.body.email, (err, rows, field) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Failed',
+        error: err
+      });
+    }
+    if (rows.length > 0) {
+      return res.status(409).json({
+        message: 'Email already exists'
+      });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err
+          });
+        } else {
+          const data = {
+            _id: nanoid(16),
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            satisfaction_rate: 0
+          };
 
-        Users.push(newUser);
+          const queryRegister = 'INSERT INTO tandur_user SET ?';
 
-        return res.status(201).json({
-          message: 'User created',
-          data: newUser
-        });
-      }
-    });
-  }
+          connection.query(queryRegister, data, (err, rows, field) => {
+            if (err) {
+              return res.status(500).json({
+                message: 'Failed',
+                error: err
+              });
+            }
+            return res.status(201).json({
+              message: 'User created',
+              data: data
+            });
+          });
+        }
+      });
+    }
+  });
 };
 
 // LOGIN
 exports.user_login = (req, res, next) => {
-  const user = Users.find(user => user.email == req.body.email)
-  if (user !== undefined) {
-    bcrypt.compare(req.body.password, user.password, (err, result) => {
-      if (err) {
-        return res.status(401).json({
-          message: 'Auth Failed'
-        });
-      }
-      if (result) {
-        const token = jwt.sign(
-          {
-            email: user.email,
-            userId: user._id
-          },
-          process.env.JWT_KEY,
-          {
-
-          }
-        );
-        return res.status(200).json({
-          message: 'Auth Success',
-          token: token
-        });
-      }
-      return res.status(401).json({
-        message: 'Auth Failed: Password is Incorrect!'
+  const queryCheckUser = 'SELECT * FROM tandur_coba.tandur_user WHERE email = ?'
+  connection.query(queryCheckUser, req.body.email, (err, rows, field) => {
+    if (err) {
+      return res.status(500).json({
+        message: 'Failed',
+        error: err
       });
-    });
-  } else {
-    return res.status(401).json({
-      message: 'Auth Failed: Email is Incorrect'
-    });
-  }
+    }
+
+    if (rows.length < 1) {
+      return res.status(401).json({
+        message: 'Email not Found'
+      });
+    } else {
+      const userData = rows[0];
+      bcrypt.compare(req.body.password, userData.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Auth Failed'
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: userData.email,
+              userId: userData._id
+            },
+            process.env.JWT_KEY,
+            {
+
+            }
+          );
+          return res.status(200).json({
+            message: 'Auth Success',
+            userId: userData._id,
+            token: token
+          });
+        }
+        return res.status(401).json({
+          message: 'Auth Failed: Password is Incorrect'
+        });
+      });
+    }
+  });
 };
 
 // GET ALL USERS
