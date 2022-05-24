8cc081a2d8eaf8077de045f9792f116f957dd3ff
atharianr <atharianrahmadani@gmail.com>
2022-05-24 09:39:36 +0700

    query zone_local and zone_city

--- src/routes/plant.js
+++ src/routes/plant.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const PlantController = require('../controllers/plant');
 const checkAuth = require('../middleware/check-auth');
 
-router.get('/:zone', checkAuth, PlantController.plant_get_nearby);
+router.get('/', checkAuth, PlantController.plant_get_nearby);
 
 module.exports = router;
