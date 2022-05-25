bb9d5535c3273b37f8cc60bbaaeec407f22959d1
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2022-05-25 08:50:05 +0700

    update code fixed_plant_detail

--- src/controllers/plant.js
+++ src/controllers/plant.js
@@ -38,9 +38,6 @@ exports.fixed_plant_detail = async (req, res, next) => {
       }
     });
 
-    // VARIABLE HARVEST DURATION
-    const duration = fixed_plant.harvest_duration;
-
     // QUERY NEARBY PLANTS
     const user_plant = await Plants.findAll({
       where: {
@@ -51,71 +48,110 @@ exports.fixed_plant_detail = async (req, res, next) => {
       }
     });
 
+    // VARIABLE HARVEST DURATION
+    const duration = fixed_plant.harvest_duration;
+
     // VARIABLE NEARBY USER PLANT
     const nearby = user_plant.length;
 
-    var d = new Date();
+    // VARIABLE START AND FINISH DATE
+    const date = new Date();
+    const start_date = date.getFullYear() + '-' + (("0" + (date.getMonth() + 1)).slice(-2)) + '-' + (("0" + date.getDate()).slice(-2));
+    const harvest_date = date.getFullYear() + '-' + (("0" + (date.getMonth() + 1 + duration)).slice(-2)) + '-' + (("0" + date.getDate()).slice(-2));
 
-    let weatherResponds;
-    var weatherRespondsArr = [];
+    // VARIABLE RESPOND
+    var humidityResponds;
+    var humidityTotal = 0.0;
+    var rainResponds;
+    var rainTotal = 0.0;
+    var tempResponds;
+    var tempTotal = 0.0;
 
     // ARRAY FOR SAVING MONTHLY DATA
-    var avgTempArr = [];
     var avgHumidityArr = [];
     var avgRainArr = [];
+    var avgTempArr = [];
 
-    // MONTHLY LOOP
-    for (let i = 0; i < duration; i++) {
-      var currentDate = d.getFullYear() + '-' + (("0" + (d.getMonth() + 1)).slice(-2)) + '-' + (("0" + d.getDate()).slice(-2));
+    // VARIABLE FOR INDEX
+    var index = 0;
+    var day = 1;
 
-      d.setMonth(d.getMonth() + 1);
+    // GET HUMIDITY DATA
+    await axios
+      .get(`https://api.meteomatics.com/${start_date}T00:00:00Z--${harvest_date}T00:00:00Z:PT24H/relative_humidity_max_2m_24h:p,precip_24h:mm,t_mean_2m_24h:C/${req.query.lat},${req.query.long}/json`, {
+        auth: {
+          username: 'tandur_shafiqrozaan',
+          password: 'RRnDv2tC81'
+        }
+      })
+      .then(res => {
+        humidityResponds = res.data.data[0].coordinates[0].dates;
+        rainResponds = res.data.data[1].coordinates[0].dates;
+        tempResponds = res.data.data[2].coordinates[0].dates;
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
-      var nextDate = d.getFullYear() + '-' + (("0" + (d.getMonth() + 1)).slice(-2)) + '-' + (("0" + d.getDate()).slice(-2));
+    // GET HUMIDITY AVG DATA
+    for (let i = 0; i < humidityResponds.length; i++) {
 
-      // GET HUMIDITY DATA
-      await axios
-        .get(`https://api.meteomatics.com/${currentDate}T00:00:00Z--${nextDate}T00:00:00Z:PT24H/relative_humidity_max_2m_24h:p/${req.query.lat},${req.query.long}/json`, {
-          auth: {
-            username: 'tandur_shafiqrozaan',
-            password: 'RRnDv2tC81'
-          }
-        })
-        .then(res => {
-          weatherResponds = res.data.data[0].coordinates[0].dates;
-          weatherRespondsArr.push(weatherResponds);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+      humidityTotal = humidityTotal + humidityResponds[i].value;
+      var date_state = humidityResponds[i].date.split("T");
 
-      var avg = 0;
-      var total = 0;
-
-      for (let j = 0; j < weatherRespondsArr[i].length; j++) {
-        total = total + weatherRespondsArr[i][j].value;
+      if (date_state[0].slice(-2) === ("0" + date.getDate()).slice(-2)) {
+        avgHumidityArr[index - 1] = humidityTotal / day;
+        humidityTotal = 0;
+        day = 0;
+        index++;
       }
+      day++;
+    }
 
-      avg = total / weatherRespondsArr[i].length;
+    index = 0;
 
-      // PUSH AVERAGE MONTHLY DATA INTO ARRAY
-      avgTempArr.push(0);
-      avgHumidityArr.push(avg);
-      avgRainArr.push(0);
+    // GET RAIN AVG DATA
+    for (let i = 0; i < rainResponds.length; i++) {
+
+      rainTotal = rainTotal + rainResponds[i].value;
+      var date_state = rainResponds[i].date.split("T");
+
+      if (date_state[0].slice(-2) === ("0" + date.getDate()).slice(-2)) {
+        avgRainArr[index - 1] = rainTotal / day;
+        rainTotal = 0;
+        day = 0;
+        index++;
+      }
+      day++;
+    }
+    
+    index = 0;
+
+    // GET TEMPERATURE AVG DATA
+    for (let i = 0; i < tempResponds.length; i++) {
+
+      tempTotal = tempTotal + tempResponds[i].value;
+      var date_state = tempResponds[i].date.split("T");
+
+      if (date_state[0].slice(-2) === ("0" + date.getDate()).slice(-2)) {
+        avgTempArr[index - 1] = tempTotal / day;
+        tempTotal = 0;
+        day = 0;
+        index++;
+      }
+      day++;
     }
 
     return res.status(200).json({
       message: 'Recommended Plant Detail',
       status: 200,
       data: {
-        location: `${req.query.zone_local}, ${req.query.zone_city}`,
+        fixed_plant: fixed_plant,
+        user_plant: user_plant,
         nearby: nearby,
-        harvest_duration: duration,
-        image_url: fixed_plant.image_url,
-        monthly_data: {
-          avg_temp: avgTempArr,
-          avg_humidity: avgHumidityArr,
-          avg_rain: avgRainArr,
-        },
+        average_humidity: avgHumidityArr,
+        average_rain: avgRainArr,
+        average_temp: avgTempArr
       }
     });
   } catch (err) {
